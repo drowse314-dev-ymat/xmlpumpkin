119c74253a3fe23c2d1e3df147a08063e3ca7911
ymat@macbookair <drowse314@gmail.com>
2014-02-10 17:27:50 +0900

    Prepare for distribuiton.
        * Isolate README/requirements loaders.
        * Add files for distribution.

---
+++ setup.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+from setuptools import setup
+
+
+def get_version():
+    return __import__('xmlpumpkin').__version__
+
+def get_readme():
+    return open('README.rst').read()
+
+def get_requirements():
+    return open('requirements.txt').read().split()
+
+setup_configs = dict(
+
+    name='xmlpumpkin',
+    version=get_version(),
+    license='BSD',
+    url='https://github.com/drowse314-dev-ymat/xmlpumpkin',
+    author='ymat',
+    author_email='drowse314@gmail.com',
+    description='CaboCha output-XML accessor',
+    long_description=get_readme(),
+    keywords='cabocha nlp xml parsing',
+    platforms=['any'],
+
+    packages=['xmlpumpkin'],
+    install_requires=get_requirements(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+    ],
+
+)
+
+
+if __name__ == '__main__':
+    setup(**setup_configs)
--- xmlpumpkin/__init__.py
+++ xmlpumpkin/__init__.py
@@ -7,6 +7,9 @@ from .tree import (
     Tree,
 )
 
+__version__ = '0.1'
+
+
 def parse_to_tree(text):
     """Parse text using CaboCha, then return Tree instance."""
     xml_text = cabocha.as_xml(text)
